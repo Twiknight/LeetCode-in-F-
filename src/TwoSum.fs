61687572b184f6b273f284cd5f453dcb2f871cc5
Twiknight <twiknight@163.com>
2016-03-02 17:56:55 +0800

    correct comment

--- src/TwoSum.fs
+++ src/TwoSum.fs
@@ -1,5 +1,5 @@
 //https://leetcode.com/problems/two-sum/
-/**
+(*
 Given an array of integers, return indices of the two numbers such that they add up to a specific target.
 
 You may assume that each input would have exactly one solution.
@@ -9,7 +9,7 @@ Given nums = [2, 7, 11, 15], target = 9,
 
 Because nums[0] + nums[1] = 2 + 7 = 9,
 return [0, 1].
-**/
+*)
 
 open System
 open System.Collections.Generic
