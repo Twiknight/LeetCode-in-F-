0c76e823e1b39ce1675e91125c6ccfdce94b52fc
TwiKnight <twiknight@163.com>
2016-03-07 23:30:24 +0800

    bug fix

--- src/Longest-Substring-Without-Repeating-Characters.fs
+++ src/Longest-Substring-Without-Repeating-Characters.fs
@@ -17,11 +17,11 @@ let lengthOfLongestSubstring (s:string) =
          if ed = arr.Length then Math.Max(ed-start,m)
          else
             let c = arr.[ed]
-            if map.ContainsKey(c) then 
-                let s1 = Math.Max(map.Item(c),start)
-                search (map.Remove(c).Add(c,s1)) (s1+1) (ed+1) (Math.Max(m,ed-s1))
-            else search (map.Add(c,ed)) start (ed+1) m
-    search Map.empty 0 1 1
+            if map.ContainsKey(c) && map.Item(c) >= start then 
+                let s1 = map.Item(c)
+                search (map.Remove(c).Add(c,ed)) (s1+1) (ed+1) (Math.Max(m,ed-s1))
+            else search (map.Add(c,ed)) start (ed+1) (Math.Max(m,ed-start+1))
+    search (Map.empty.Add(arr.[0],0)) 0 1 1
 
 [<EntryPoint>]
 let main argv=
